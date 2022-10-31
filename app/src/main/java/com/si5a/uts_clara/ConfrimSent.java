253194fcc3351b98e590a3caad763e05d5f02bc2
Riza Umami <clarameyhazlinda@mhs.mdp.ac.id>
2022-10-31 22:17:42 +0700

    Update Yang terbaru

--- app/src/main/java/com/si5a/uts_clara/ConfrimSent.java
+++ app/src/main/java/com/si5a/uts_clara/ConfrimSent.java
@@ -4,32 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class ConfrimSent extends AppCompatActivity {
 
+    String Namalengkap, Nomorpendaftaran, Jalurpendaftaran;
+
+    TextView tvNama, tvNomor, tvJalur;
+    Spinner sp_jalur;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confrim_sent);
 
-        String nama, nomor, jalur;
-
-        TextView tvNama, tvNomor, tvOpt;
-
         tvNama = findViewById(R.id.tvNama);
         tvNomor = findViewById(R.id.tvNomor);
-        tvOpt = findViewById(R.id.tvOpt);
+        tvJalur = findViewById(R.id.tvJalur);
 
-        Intent terima = getIntent();
+        Intent intent = getIntent();
 
-        nama = terima.getStringExtra("VarNama");
-        nomor = terima.getStringExtra("VarNomor");
-        jalur = terima.getStringExtra("VarJalur");
-
-        tvNama.setText(nama);
-        tvNomor.setText(nomor);
-        tvOpt.setText(jalur);
+        tvNama.setText(intent.getStringExtra("VarNamaPendaftaran"));
+        tvNomor.setText(intent.getStringExtra("VarNomorPendaftaran"));
+        tvJalur.setText(intent.getStringExtra("VarJalurPendaftaran"));
 
 
 
--- app/src/main/java/com/si5a/uts_clara/MainActivity.java
+++ app/src/main/java/com/si5a/uts_clara/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     CheckBox cb_konfir;
     Spinner sp_jalur;
 
-    String nama, nomor, jalur;
+    String Namalengkap, Nomorpendaftaran, Jalurpendaftaran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,22 +36,33 @@ public class MainActivity extends AppCompatActivity {
         btn_konfir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nama = et_nama.getText().toString();
-                nomor = et_nomor.getText().toString();
-//                jalur = sp_jalur.getOnItemClickListener();
+                Namalengkap = et_nama.getText().toString();
+                Nomorpendaftaran = et_nomor.getText().toString();
+                Jalurpendaftaran = sp_jalur.getSelectedItem().toString();
 
 
-                if (nama.trim().equals("")){
+                if (Namalengkap.trim().equals("")){
                     et_nama.setError("Nama Tidak Boleh Kosong");
                 }
-                else if (nomor.trim().equals("")){
+                else if (Nomorpendaftaran.trim().equals("")){
                     et_nomor.setError("Nomor Pendaftaran Tidak boleh kosong");
                 } else{
-                    Intent sent = new Intent(MainActivity.this, ConfrimSent.class);
-                    sent.putExtra("VarNama", String.valueOf(et_nama));
-                    sent.putExtra("VarNomor", String.valueOf(et_nomor));
-                    sent.putExtra("VarJalur", String.valueOf(sp_jalur));
-                    startActivity(sent);
+                    Intent kirim = new Intent(MainActivity.this, ConfrimSent.class);
+                    kirim.putExtra("VarNamaPendaftaran", String.valueOf(Namalengkap));
+                    kirim.putExtra("VarNomorPendaftaran", String.valueOf(Nomorpendaftaran));
+                    kirim.putExtra("VarJalurPendaftaran", String.valueOf(Jalurpendaftaran));
+                    startActivity(kirim);
+                }
+            }
+        });
+
+        cb_konfir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cb_konfir.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Checkbox is Checked", Toast.LENGTH_SHORT).show();
+                } else if (!cb_konfir.isChecked()){
+                    Toast.makeText(MainActivity.this, "Checkbox is not Checked", Toast.LENGTH_SHORT).show();
                 }
             }
         });
