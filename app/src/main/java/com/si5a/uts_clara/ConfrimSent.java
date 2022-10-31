fc36b50d568358c0e7ca0ff1b5064420455e4ffc
Riki Chandra <114376656+clarameyhazlinda@users.noreply.github.com>
2022-10-31 16:49:01 +0700

    Update
    Bissmilah

---
+++ app/src/main/java/com/si5a/uts_clara/ConfrimSent.java
@@ -0,0 +1,37 @@
+package com.si5a.uts_clara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ConfrimSent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confrim_sent);
+
+        String nama, nomor, jalur;
+
+        TextView tvNama, tvNomor, tvOpt;
+
+        tvNama = findViewById(R.id.tvNama);
+        tvNomor = findViewById(R.id.tvNomor);
+        tvOpt = findViewById(R.id.tvOpt);
+
+        Intent terima = getIntent();
+
+        nama = terima.getStringExtra("VarNama");
+        nomor = terima.getStringExtra("VarNomor");
+        jalur = terima.getStringExtra("VarJalur");
+
+        tvNama.setText(nama);
+        tvNomor.setText(nomor);
+        tvOpt.setText(jalur);
+
+
+
+    }
+}
